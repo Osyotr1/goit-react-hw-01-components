a8ab1fd5266a828dac8aa4221faf1c87bb882c5d
Osyotr1 <92748388+Osyotr1@users.noreply.github.com>
2022-08-11 00:06:28 +0300

    statsstart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,20 @@
 import Profile from './Profile/Profile';
+import StatList from './Statistics/StatList';
 import user from './Profile/user.json';
+import data from './Statistics/data.json';
 
 export const App = () => {
   return (
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101'
-    //   }}
-    // >
-    //   React homework template
-    // </div>
-    <>
-       <Profile
+    <div>
+      <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-        />
-    </>
+      />
+      <StatList title="Upload stats" stats={data} />     
+    </div>
   );
 };
 
